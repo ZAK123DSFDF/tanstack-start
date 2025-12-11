ba417ca8faf1c5c6d7d158c8f091e1f34b2eecd7
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-12-11 15:39:16 +0300

    add joke route with server function to fetch random jokes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,6 +13,10 @@ import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 import appCss from '../styles.css?url'
 
 import type { QueryClient } from '@tanstack/react-query'
+import {ReactNode} from "react";
+
+
+
 
 interface MyRouterContext {
   queryClient: QueryClient
@@ -43,7 +47,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   shellComponent: RootDocument,
 })
 
-function RootDocument({ children }: { children: React.ReactNode }) {
+function RootDocument({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
